55432b598c6d32c982a81aba079b52c9d2084a9f
cereceres <jesus.edelcereceres@gmail.com>
2018-03-11 23:36:04 -0700

    the test are ready

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ arxiv api client to node
 
 # API 
 
-# ARXIV-API-NODE.query(queryStringToArxiv)
+### ARXIV-API-NODE.query({search_query,id_list,start,max_results})
 
 return a promise with body response from Arxiv
--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const {promisify} = require('util')
 const get = promisify(request.get.bind(request))
 
 
-const uri = 'http://export.arxiv.org/api/query'
+const uri = 'http://export.arxiv.org/api'
 
-exports.query = (query) => get({
-    uri,
+exports.query = ({
+    search_query,
+	id_list,
+	start,
+	max_results
+}) => get({
+    uri: uri+'/query',
     qs:{
-        search_query: query
+        search_query,
+        id_list,
+        start,
+        max_results
     }
 })
     .then(({body})=> body)
